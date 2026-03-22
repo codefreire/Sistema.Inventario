522f0263ba6806e9c2640fe9b74bdbc763c88656
Carlos Freire <cestebanfreire@hotmail.com>
2026-03-21 23:17:53 -0500

    Feature: Integrar Serilog para el registro de logs en un archivo log-fecha.txt y actualizar configuración de logging para Productos

--- Backend/Sistema.Inventario.Producto/Program.cs
+++ Backend/Sistema.Inventario.Producto/Program.cs
@@ -1,7 +1,11 @@
 using Sistema.Inventario.Producto.API.Extensiones;
 using Sistema.Inventario.Producto.API.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+builder.Logging.AddSerilog(Log.Logger, dispose: true);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
