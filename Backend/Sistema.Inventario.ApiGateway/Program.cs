768915b4ed02bca6b95a354e071c0a8f7395fab6
Carlos Freire <cestebanfreire@hotmail.com>
2026-03-21 20:34:04 -0500

    Feature: agregar ApiGateway con Ocelot y health check

---
+++ Backend/Sistema.Inventario.ApiGateway/Program.cs
@@ -0,0 +1,25 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+app.UseHealthChecks("/health");
+
+await app.UseOcelot();
+
+app.Run();
