fa38f914c7abd81bb8252ba2fe363fa0d2c114b1
Carlos Freire <cestebanfreire@hotmail.com>
2026-04-06 11:06:03 -0500

    Feature: Agregar migración inicial para InventarioProductosBD

---
+++ Backend/Microservicios/Sistema.Inventario.Producto/Migrations/20260406155412_EsquemaInicialInventarioProductosBD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema.Inventario.Producto.Migrations
+{
+    /// <inheritdoc />
+    public partial class EsquemaInicialInventarioProductosBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Categoria = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                    table.CheckConstraint("CK_Productos_Precio", "Precio >= 0");
+                    table.CheckConstraint("CK_Productos_Stock", "Stock >= 0");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
